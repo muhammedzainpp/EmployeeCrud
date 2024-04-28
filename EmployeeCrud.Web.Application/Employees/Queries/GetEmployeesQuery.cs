255a2a847166613b9cfc92e6ac722766e4f13470
muhammedzainpp <muhammedzain848@gmail.com>
2024-04-28 23:10:22 +0530

    Add GetEmployees Query

---
+++ EmployeeCrud.Web.Application/Employees/Queries/GetEmployeesQuery.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeCrud.Web.Application.Interfaces;
+using EmployeeCrud.Web.Shared.Dtos;
+using EmployeeCrud.Web.Shared.Responses;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static EmployeeCrud.Web.Application.Helpers.ResponseHelpers;
+
+
+namespace EmployeeCrud.Web.Application.Employees.Queries;
+public class GetEmployeesQuery : IRequest<Response<IEnumerable<EmployeeDto>>>
+{ 
+}
+public class GetEmployeesQueryHandler(IAppDbContext context) : IRequestHandler<GetEmployeesQuery, Response<IEnumerable<EmployeeDto>>>
+{
+    private readonly IAppDbContext _context = context;
+
+    public async Task<Response<IEnumerable<EmployeeDto>>> Handle(GetEmployeesQuery request, CancellationToken cancellationToken)
+    {
+        return await TryHandleAsync();
+    }
+    private async Task<Response<IEnumerable<EmployeeDto>>> TryHandleAsync()
+    {
+        Response<IEnumerable<EmployeeDto>> response;
+        try
+        {
+            var employees = await GetEmployeesAsync();
+            response = OnSuccess<IEnumerable<EmployeeDto>>(employees);
+        }
+        catch (Exception ex)
+        {
+            response = OnError<IEnumerable<EmployeeDto>>(ex);
+        }
+        return response;
+    }
+
+    private async Task<IEnumerable<EmployeeDto>> GetEmployeesAsync()
+    {
+        return await _context
+                   .Employees
+                   .Select(x => new EmployeeDto
+                   {
+                   Id = x.Id,
+                   Name = x.Name,
+                   Description = x.Description,
+                   })
+                   .ToListAsync();
+    }
+}
+
+
---
+++ EmployeeCrud.Web.Shared/Dtos/EmployeeDto.cs
@@ -0,0 +1,7 @@
+﻿namespace EmployeeCrud.Web.Shared.Dtos;
+public class EmployeeDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; } 
+    public required string Description { get; set; }
+}
