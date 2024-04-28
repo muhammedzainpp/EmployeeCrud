ae6d5b04e93a3a94e53da8df16f04523a66bf1f3
muhammedzainpp <muhammedzain848@gmail.com>
2024-04-28 22:47:06 +0530

    Add SaveEmployee method in Api

--- EmployeeCrud.Web/EmployeeCrud.Web/Apis/EmployeeApi.cs
+++ EmployeeCrud.Web/EmployeeCrud.Web/Apis/EmployeeApi.cs
@@ -1,9 +1,21 @@
-﻿namespace EmployeeCrud.Web.Apis;
+﻿
+using EmployeeCrud.Web.Application.Employees.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeCrud.Web.Apis;
 
 public static class EmployeeApi
 {
     public static void MapEmployees(this RouteGroupBuilder app)
     {
         var group = app.MapGroup("employees");
+        group.MapPost("/",SaveEmployee);
+    }
+
+    private static async Task<IResult> SaveEmployee([FromServices] IMediator mediator , [FromBody] SaveEmployeeCommand request)
+    {
+        var response = await mediator.Send(request);
+        return Results.Ok(response);
     }
 }
