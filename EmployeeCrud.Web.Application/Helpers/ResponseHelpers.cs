69aae6838bf9e87092b87e9b2dbba897a864466d
muhammedzainpp <muhammedzain848@gmail.com>
2024-04-28 22:13:53 +0530

    Add Rsponse Class

---
+++ EmployeeCrud.Web.Application/Helpers/ResponseHelpers.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeCrud.Web.Shared.Responses;
+
+namespace EmployeeCrud.Web.Application.Helpers;
+public class ResponseHelpers
+{
+    public static Response<T> OnSuccess<T>(T result)
+     => new Response<T> { IsSuccess = true, Result = result };
+
+    public static Response<T> OnError<T>(Exception ex)
+     => new Response<T> { IsSuccess = false, Errors = new[] { ex.Message } };
+}
+
---
+++ EmployeeCrud.Web.Shared/Responses/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeCrud.Web.Shared.Responses;
+public class Response<T>
+{
+    public bool IsSuccess { get; set; }
+    public T? Result { get; set; }
+    public IEnumerable<string>? Errors { get; set; }
+
+}
