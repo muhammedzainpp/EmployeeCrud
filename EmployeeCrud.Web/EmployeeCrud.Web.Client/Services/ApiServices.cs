3060aadf06cee78e51066203c9aa05c23a32216b
muhammedzainpp <muhammedzain848@gmail.com>
2024-04-29 22:20:29 +0530

    Add ApiServices TO Te Client

---
+++ EmployeeCrud.Web/EmployeeCrud.Web.Client/Services/ApiServices.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeCrud.Web.Shared.Responses;
+using System.Net.Http.Json;
+
+namespace EmployeeCrud.Web.Client.Services;
+
+public class ApiServices(HttpClient http) : IApiServices
+{
+    private readonly HttpClient http = http;
+
+    public async Task<Response<T>> Get<T>(string url)
+    {
+        var response = await http.GetFromJsonAsync<Response<T>>(url) ?? default!;
+        return response;
+    }
+
+    public async Task<Response<T>> GetById<T>(string url, int id)
+    {
+        var response = await http.GetFromJsonAsync<Response<T>>($"{url}/{id}") ?? default!;
+        return response;
+    }
+
+    public async Task<Response<int>> Post<T>(string url, T request)
+    {
+        var response = await http.PostAsJsonAsync(url, request) ?? default!;
+        return await response.Content.ReadFromJsonAsync<Response<int>>() ?? default!;
+    }
+
+}
---
+++ EmployeeCrud.Web/EmployeeCrud.Web.Client/Services/IApiServices.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeCrud.Web.Shared.Responses;
+
+namespace EmployeeCrud.Web.Client.Services;
+
+public interface IApiServices
+{
+    Task<Response<T>> Get<T>(string url);
+    Task<Response<T>> GetById<T>(string url, int id);
+    Task<Response<int>> Post<T>(string url, T request);
+}
